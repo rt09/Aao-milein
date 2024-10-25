29586b421381ab70a4aff9cfcceaadf60639d5f0
rt09 <78974453+rt09@users.noreply.github.com>
2024-10-25 23:57:36 +0530

    ch

---
+++ frontend/src/components/room.tsx
@@ -0,0 +1,13 @@
+import { useEffect, useState } from 'react'
+import { useSearchParams } from "react-router-dom";
+export const Room = () => {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const name = searchParams.get('name');
+
+    useEffect(() => {
+    },[name])
+
+    return <div>
+       hi {name}
+    </div>
+}
